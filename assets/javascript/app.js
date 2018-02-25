d54567288bc1e36ec75ba8e493bfd2753a58725a
Wesley Hanson <wrh1600@gmail.com>
2018-02-25 00:19:31 -0700

    displaying rating and gif

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -11,17 +11,48 @@ var emotions = ["Happy", "Sad", "Mad"];
 	          url: queryURL,
 	          method: "GET"
 	        }).then(function(response) {
-	        	console.log(response.data[0]);
+	        	
 	        	var emotionDiv = $("<div class='emotion'>");
 	        	var rating = response.data[0].rating;
 	        	var p1 = $("<p>").text(rating);
-	        	emotionDiv.html(p1);
-
-	        	var giphUrl = response.data[0].bitly_gif_url 
+	        	emotionDiv.append(p1);
+				var giphUrl = response.data[0].images.fixed_height.url;
 	        	var giph = $("<img>").attr("src", giphUrl);
 	        	emotionDiv.append(giph);
 
-	        	$("#emotion-view").prepend(emotionDiv);
+	        	
+	        	var rating1 = response.data[1].rating;
+	        	var p2 = $("<p>").text(rating1);
+	        	emotionDiv.append(p2);
+				var giphUrl1 = response.data[1].images.fixed_height.url;
+	        	var giph1 = $("<img>").attr("src", giphUrl1);
+	        	emotionDiv.append(giph1);
+
+	        	var rating2 = response.data[2].rating;
+	        	var p3 = $("<p>").text(rating2);
+	        	emotionDiv.append(p3);
+				var giphUrl2 = response.data[2].images.fixed_height.url;
+	        	var giph2 = $("<img>").attr("src", giphUrl2);
+	        	emotionDiv.append(giph2);
+
+	        	var rating3 = response.data[3].rating;
+	        	var p4 = $("<p>").text(rating1);
+	        	emotionDiv.append(p4);
+				var giphUrl3 = response.data[3].images.fixed_height.url;
+	        	var giph3 = $("<img>").attr("src", giphUrl3);
+	        	emotionDiv.append(giph3);
+
+	        	var rating4 = response.data[4].rating;
+	        	var p5 = $("<p>").text(rating4);
+	        	emotionDiv.append(p5);
+				var giphUrl4 = response.data[4].images.fixed_height.url;
+	        	var giph4 = $("<img>").attr("src", giphUrl4);
+	        	emotionDiv.append(giph4);
+
+	        	
+
+
+	        	$("#emotion-view").html(emotionDiv);
 	          
 	        });
 
