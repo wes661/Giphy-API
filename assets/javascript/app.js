286824089f2ee4f34e1b5441ee3dfea603b12a6f
Wesley Hanson <wrh1600@gmail.com>
2018-02-25 19:17:34 -0700

    updated code, for loop is commented out

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,50 +4,53 @@ var emotions = ["Happy", "Sad", "Mad"];
 	    function displayEmotion() {
 
 	        var emotion = $(this).attr("data-name");
-	        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + emotion + "&api_key=DHsjRmQevQ0JKl36B2yOJJUp85Zt0VvF";
+	        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + emotion + "&limit=10&api_key=DHsjRmQevQ0JKl36B2yOJJUp85Zt0VvF";
 
-	        // Creating an AJAX call for the specific movie button being clicked
+	        
 	        $.ajax({
 	          url: queryURL,
 	          method: "GET"
 	        }).then(function(response) {
-	        	
+	        	for(var i = 0; i < response.data.length; i++ ){
+	        		console.log(response.data[i].rating);
+
 	        	var emotionDiv = $("<div class='emotion'>");
-	        	var rating = response.data[0].rating;
-	        	var p1 = $("<p>").text(rating);
+	        	var rating = response.data[i].rating;
+	        	var p1 = $("<p>").text(rating[i]);
 	        	emotionDiv.append(p1);
-				var giphUrl = response.data[0].images.fixed_height.url;
+				var giphUrl = response.data[i].images.fixed_height.url;
 	        	var giph = $("<img>").attr("src", giphUrl);
 	        	emotionDiv.append(giph);
+	        	}
 
 	        	
-	        	var rating1 = response.data[1].rating;
-	        	var p2 = $("<p>").text(rating1);
-	        	emotionDiv.append(p2);
-				var giphUrl1 = response.data[1].images.fixed_height.url;
-	        	var giph1 = $("<img>").attr("src", giphUrl1);
-	        	emotionDiv.append(giph1);
+	   //      	var rating1 = response.data[1].rating;
+	   //      	var p2 = $("<p>").text(rating1);
+	   //      	emotionDiv.append(p2);
+				// var giphUrl1 = response.data[1].images.fixed_height.url;
+	   //      	var giph1 = $("<img>").attr("src", giphUrl1);
+	   //      	emotionDiv.append(giph1);
 
-	        	var rating2 = response.data[2].rating;
-	        	var p3 = $("<p>").text(rating2);
-	        	emotionDiv.append(p3);
-				var giphUrl2 = response.data[2].images.fixed_height.url;
-	        	var giph2 = $("<img>").attr("src", giphUrl2);
-	        	emotionDiv.append(giph2);
+	   //      	var rating2 = response.data[2].rating;
+	   //      	var p3 = $("<p>").text(rating2);
+	   //      	emotionDiv.append(p3);
+				// var giphUrl2 = response.data[2].images.fixed_height.url;
+	   //      	var giph2 = $("<img>").attr("src", giphUrl2);
+	   //      	emotionDiv.append(giph2);
 
-	        	var rating3 = response.data[3].rating;
-	        	var p4 = $("<p>").text(rating1);
-	        	emotionDiv.append(p4);
-				var giphUrl3 = response.data[3].images.fixed_height.url;
-	        	var giph3 = $("<img>").attr("src", giphUrl3);
-	        	emotionDiv.append(giph3);
+	   //      	var rating3 = response.data[3].rating;
+	   //      	var p4 = $("<p>").text(rating1);
+	   //      	emotionDiv.append(p4);
+				// var giphUrl3 = response.data[3].images.fixed_height.url;
+	   //      	var giph3 = $("<img>").attr("src", giphUrl3);
+	   //      	emotionDiv.append(giph3);
 
-	        	var rating4 = response.data[4].rating;
-	        	var p5 = $("<p>").text(rating4);
-	        	emotionDiv.append(p5);
-				var giphUrl4 = response.data[4].images.fixed_height.url;
-	        	var giph4 = $("<img>").attr("src", giphUrl4);
-	        	emotionDiv.append(giph4);
+	   //      	var rating4 = response.data[4].rating;
+	   //      	var p5 = $("<p>").text(rating4);
+	   //      	emotionDiv.append(p5);
+				// var giphUrl4 = response.data[4].images.fixed_height.url;
+	   //      	var giph4 = $("<img>").attr("src", giphUrl4);
+	   //      	emotionDiv.append(giph4);
 
 	        	
 
@@ -66,6 +69,8 @@ var emotions = ["Happy", "Sad", "Mad"];
 
 	        // Looping through the array of movies
 	        for (var i = 0; i < emotions.length; i++) {
+
+	        	console.log(emotions[i])
 
 	          // Then dynamicaly generating buttons for each movie in the array
 	          // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
